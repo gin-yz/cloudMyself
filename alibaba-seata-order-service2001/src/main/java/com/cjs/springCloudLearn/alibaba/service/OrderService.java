6678267857e186423bab2e04cca0315363387e18
‘liquidFox’ <ginyz@foxmail.com>
2021-05-19 22:48:25 +0800

    update in 20210519

---
+++ alibaba-seata-order-service2001/src/main/java/com/cjs/springCloudLearn/alibaba/service/OrderService.java
@@ -0,0 +1,12 @@
+package com.cjs.springCloudLearn.alibaba.service;
+
+import com.cjs.springCloudLearn.alibaba.domain.Order;
+
+/**
+ * @auther zzyy
+ * @create 2020-02-26 15:19
+ */
+public interface OrderService
+{
+    void create(Order order);
+}
